c50db43f524704229fdfdaa3afbed477f37d8482
Kushal Krishna <kushal5070@gmail.com>
2022-09-07 20:14:30 +0530

    google authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,16 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+
+    const logGoogleUser = async () => {
+        const response = await signInWithGooglePopup();
+    } 
+
     return (
         <>
-        
+            <div>
+                <button onClick={logGoogleUser}>Sign In with Google</button>
+            </div>
         </>
     );
 }
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBD0xy5n2fFYi_BUB0uyqH_tyRwOJk1-2k",
+    authDomain: "my-ecomm-da22b.firebaseapp.com",
+    projectId: "my-ecomm-da22b",
+    storageBucket: "my-ecomm-da22b.appspot.com",
+    messagingSenderId: "255916108468",
+    appId: "1:255916108468:web:a126f32c2676777a3a85e1"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider();
+
+  provider.setCustomParameters({
+    prompt: "select_account"
+  });
+
+  export const auth = getAuth();
+
+  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
