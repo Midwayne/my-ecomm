13e197750347718ec8db5d1188677e66ac52f623
Kushal Krishna <kushal5070@gmail.com>
2022-09-07 20:34:25 +0530

    database

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+
 
 const SignIn = () => {
 
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopup();
+        createUserDocumentFromAuth(response)
     } 
 
     return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -2,6 +2,12 @@ import { initializeApp } from 'firebase/app';
 
 import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
 
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
+
+
+
+//auth
+
 const firebaseConfig = {
     apiKey: "AIzaSyBD0xy5n2fFYi_BUB0uyqH_tyRwOJk1-2k",
     authDomain: "my-ecomm-da22b.firebaseapp.com",
@@ -23,3 +29,15 @@ const firebaseConfig = {
   export const auth = getAuth();
 
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+
+
+  //database
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+  };
